8c7523750473d5d691d8b58635103c90cbcc9fe0
Jose Chavez <josmchavz@gmail.com>
2024-12-01 17:35:47 -0800

    middleware no landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Link from "next/link"
-import {Box, Button, Container, Grid, Section, Text} from "@radix-ui/themes";
+import {Box, Container, Grid, Heading, Section, Text} from "@radix-ui/themes";
 import Image from "next/image";
 
 export default function Page() {
@@ -23,11 +23,11 @@ export default function Page() {
                             className="mx-auto grayscale aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
                         />
                         <div className="flex flex-col justify-center space-y-4">
-                            <div className="space-y-2">
+                            <div>
                                 <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                                     The complete platform to keep your money up
                                 </h1>
-                                <Text as={'p'} className="my-8 max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                                <Text as={'p'} className="my-4 max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
                                     Give your team the education that they deserve. With thousands of courses,
                                     you can elevate your mind. Get ya money up not ya funny up.
                                 </Text>
@@ -55,39 +55,35 @@ export default function Page() {
             <Section className="w-full py-12">
                 <Container>
                     <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                        <div className="space-y-2">
+                        <div className={'mb-8'}>
                             <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                                New Features
+                                New Courses Every Week
                             </div>
-                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Faster iteration. More
-                                innovation.</h2>
-                            <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                                The platform for rapid progress. Let your team focus on shipping features instead of
-                                managing
-                                infrastructure with automated CI/CD, built-in testing, and integrated collaboration.
-                            </p>
+                            <Heading as={'h2'} className="text-3xl font-bold tracking-tighter sm:text-5xl mb-4">Maxxing Learning Stats.</Heading>
+                            <Text as={'p'} className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                                The platform for rapid big brain progress. Let your team focus on catching up with the latest.
+                                Trust us, we know what we&#39;re doing, unlike Coursera.
+                            </Text>
                         </div>
                     </div>
                     <div
                         className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
                         <div className="grid gap-1">
-                            <h3 className="text-lg font-bold">Infinite scalability, zero config</h3>
+                            <h3 className="text-lg font-bold">Infinite knowledge</h3>
                             <p className="text-sm text-gray-500 dark:text-gray-400">
-                                Enable code to run on-demand without needing to manage your own infrastructure or
-                                upgrade hardware.
+                                Become the smartest person in the room with our infinite knowledge.
                             </p>
                         </div>
                         <div className="grid gap-1">
-                            <h3 className="text-lg font-bold">Real-time insights and controls</h3>
+                            <h3 className="text-lg font-bold">Better than Neuralink</h3>
                             <p className="text-sm text-gray-500 dark:text-gray-400">
-                                Get granular, first-party, real-user metrics on site performance per deployment.
+                                Don&#39;t let Elon Musk control your brain. Use our platform instead.
                             </p>
                         </div>
                         <div className="grid gap-1">
-                            <h3 className="text-lg font-bold">Personalization at the edge</h3>
+                            <h3 className="text-lg font-bold">Edging to learn</h3>
                             <p className="text-sm text-gray-500 dark:text-gray-400">
-                                Deliver dynamic, personalized content, while ensuring users only see the best version of
-                                your site.
+                                The only platform that will make you want to learn more 24/7.
                             </p>
                         </div>
                     </div>
