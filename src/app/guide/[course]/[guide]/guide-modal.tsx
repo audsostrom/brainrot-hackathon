6555b0696c4172ada6dea155187fe8270452dba1
Mialaniz <michaelalaniz76@gmail.com>
2024-12-01 19:00:48 -0800

    dialog box

--- src/app/guide/[course]/[guide]/guide-modal.tsx
+++ src/app/guide/[course]/[guide]/guide-modal.tsx
@@ -31,18 +31,18 @@ export default function ConfirmationModal({ onSubmit, isOpen, onClose, courseId,
    <Dialog.Root open={isOpen} onOpenChange={onClose}>
      <Dialog.Portal>
        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-       <Dialog.Content className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg z-50 h-[300px]">
-         <Dialog.Title>
+       <Dialog.Content className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-600 p-6 rounded-lg shadow-lg z-50 h-[300px]">
+         <Dialog.Title className="text-black">
          Submit Code for Review
 
          </Dialog.Title>
-         <Dialog.Description>
+         <Dialog.Description className="text-black">
             Are you sure you want to submit your code for review? This will assess your code.
          </Dialog.Description>
          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-         <div className="mt-4 flex justify-end gap-2">
+         <div className="mt-36 flex justify-end gap-2">
            <Dialog.Close asChild>
-             <button className="bg-gray-300 px-4 py-2 rounded">Cancel</button>
+             <button className="bg-gray-300 text-black px-4 py-2 rounded">Cancel</button>
            </Dialog.Close>
            <button
              className="bg-[#3e63dd] text-white px-4 py-2 rounded"
